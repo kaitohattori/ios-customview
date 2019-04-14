aa4e8085230b79f91e3086af9830fda2c57d6180
kaitohattori <kaichan394@gmail.com>
2019-04-14 15:07:24 +0900

    Initial Commit

---
+++ customview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customview
+//
+//  Created by kaichan on 4/14/19.
+//  Copyright © 2019 Kaito Hattori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
